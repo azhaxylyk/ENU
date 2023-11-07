58c96cb72f8d3f90c21f2f23a3cfa11158f86217
azhaxylyk <zhaxylykova.a7@gmail.com>
2023-11-07 19:34:49 +0600

    bfesnoivfin

--- MOIS_tasks/10th week/shy.py
+++ MOIS_tasks/10th week/shy.py
@@ -1,54 +1,23 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
-from sklearn.ensemble import AdaBoostRegressor
-import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import GradientBoostingRegressor
 
-# Загрузка данных из Excel-файла
-df = pd.read_excel('C:/Tasbaqa/5/Mathematicalfoundationsofintelligentsystems/sro/Kaz/12.xlsx')
+df = pd.read_csv('C:/Tasbaqa/5/ENU/MOIS_tasks/10th week/sarysu_grace.csv')
+df['datetime'] = pd.to_datetime(df['datetime'], format='%d/%m/%Y')
+df['datetime'] = (df['datetime'] - df['datetime'].min()).dt.days
 
-# Выбор признаков и целевой переменной
-X = df[['город', 'Тип места']]  # Признаки: Название города и тип места
-y = df['Количество домовладений']  # Целевая переменная: Количество домохозяйств
+df = pd.get_dummies(df, columns=['region'], drop_first=True)
+df.fillna(df.mean(), inplace=True)
 
-# Преобразование категориальных признаков в числовые с помощью One-Hot Encoding
-X = pd.get_dummies(X)
+X = df.drop(columns=['CSR', 'JPL', 'GFZ']) 
+y = df['CSR']
 
-# Разделение данных на обучающую и тестовую выборки
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Обучение модели SVR
-svm_model = SVR()
-svm_model.fit(X_train, y_train)
+gbm_model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, random_state=42)
+gbm_model.fit(X_train, y_train)
 
-# Оценка точности модели SVR
-svm_accuracy = svm_model.score(X_test, y_test)
-print(f"Точность модели SVR: {svm_accuracy}")
-
-# Обучение модели AdaBoost
-adaboost_model = AdaBoostRegressor()
-adaboost_model.fit(X_train, y_train)
-
-# Оценка точности модели AdaBoost
-adaboost_accuracy = adaboost_model.score(X_test, y_test)
-print(f"Точность модели AdaBoost: {adaboost_accuracy}")
-
-# Построение графиков
-plt.figure(figsize=(10, 5))
-
-# График предсказанных значений от реальных для модели SVR
-plt.subplot(1, 2, 1)
-plt.scatter(y_test, svm_model.predict(X_test))
-plt.xlabel('Реальные значения')
-plt.ylabel('Предсказанные значения')
-plt.title('SVR')
-
-# График предсказанных значений от реальных для модели AdaBoost
-plt.subplot(1, 2, 2)
-plt.scatter(y_test, adaboost_model.predict(X_test))
-plt.xlabel('Реальные значения')
-plt.ylabel('Предсказанные значения')
-plt.title('AdaBoost')
-
-plt.tight_layout()
-plt.show()
+gbm_predictions = gbm_model.predict(X_test)
+gbm_rmse = mean_squared_error(y_test, gbm_predictions, squared=False)
+print(f"RMSE (Gradient Boosting - CSR): {gbm_rmse}")
--- MOIS_tasks/10th week/xgb.py
+++ MOIS_tasks/10th week/xgb.py
@@ -1,36 +1,24 @@
-# Импортируем необходимые библиотеки
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import xgboost as xgb
 
-# Загрузка данных из CSV-файла
-df = pd.read_csv('C:/Tasbaqa/5/Mathematicalfoundationsofintelligentsystems/sro/Kaz/sarysu_grace.csv')
+df = pd.read_csv('C:/Tasbaqa/5/ENU/MOIS_tasks/10th week/sarysu_grace.csv')
 
-# Преобразование столбца "datetime" в формат даты
 df['datetime'] = pd.to_datetime(df['datetime'], format='%d/%m/%Y')
-
-# Преобразование столбца "datetime" в числовой формат (количество дней с начала периода)
 df['datetime'] = (df['datetime'] - df['datetime'].min()).dt.days
 
-# Применение One-Hot Encoding к столбцу "region"
 df = pd.get_dummies(df, columns=['region'], drop_first=True)
-
-# Заполнение пропущенных значений средними значениями
 df.fillna(df.mean(), inplace=True)
 
-# Подготовка данных
 X = df.drop(columns=['CSR', 'JPL', 'GFZ'])  # Исключаем целевые переменные из признаков
 y = df[['CSR', 'JPL', 'GFZ']]
 
-# Разделение данных на обучающий и тестовый наборы
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Обучение модели с использованием XGBoost
 xgb_model = xgb.XGBRegressor(objective='reg:squarederror')
 xgb_model.fit(X_train, y_train)
 
-# Прогнозирование и оценка модели XGBoost
 xgb_predictions = xgb_model.predict(X_test)
 xgb_rmse = mean_squared_error(y_test, xgb_predictions, squared=False)
 print(f"RMSE (XGBoost): {xgb_rmse}")
