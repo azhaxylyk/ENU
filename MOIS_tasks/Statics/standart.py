4f8a57da943d68bda4d241218cca3cc0a223e485
azhaxylyk <zhaxylykova.a7@gmail.com>
2023-11-22 09:48:26 +0600

    standart

--- MOIS_tasks/Statics/standart.py
+++ MOIS_tasks/Statics/standart.py
@@ -3,6 +3,6 @@ import numpy as np
 
 full_health_data = pd.read_csv("C:/Car/5/ENU/MOIS_tasks/Statics/data.csv", header=0, sep=",")
 
-std = np.std(full_health_data)
+std = np.std(full_health_data, axis=0)
 
 print(std)
