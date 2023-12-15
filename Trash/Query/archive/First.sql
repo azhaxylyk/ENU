e8173f9b747f00f4734644d0015b684c9177a422
azhaxylyk <zhaxylykova.a7@gmail.com>
2023-12-15 14:30:21 +0600

    New formatting

--- Trash/Query/archive/First.sql
+++
@@ -1,4 +0,0 @@
-SELECT e.Employee_ID, e.Employee_Name, p.Position_Name
-FROM LibEmployees e
-JOIN LibPositions p ON e.Position_ID = p.Position_ID
-WHERE p.Position_Name = 'Bibliographer'
--- library-management/try/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\tasbaqa\library-management\library-management\try\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
